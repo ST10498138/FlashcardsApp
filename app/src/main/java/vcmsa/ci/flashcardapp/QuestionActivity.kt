178cb261289261afd47e7892d5248acd8a0814f6
ST10498138 <ST10498138@imconnect.edu.za>
2025-05-06 13:13:45 +0200

    initial commit of the project

---
+++ app/src/main/java/vcmsa/ci/flashcardapp/QuestionActivity.kt
@@ -0,0 +1,23 @@
+package vcmsa.ci.flashcardapp
+
+import android.os.Bundle
+import androidx.activity.enableEdgeToEdge
+import androidx.appcompat.app.AppCompatActivity
+import androidx.core.view.ViewCompat
+import androidx.core.view.WindowInsetsCompat
+
+class QuestionActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        enableEdgeToEdge()
+        setContentView(R.layout.QuestionActivity)
+
+
+
+        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
+            val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
+            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
+            insets
+        }
+    }
+}
